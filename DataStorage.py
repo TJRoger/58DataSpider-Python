264d2f70073fe5ca53a49386cc681c0e6997f07f
Roger Luo <tjroger@tjroger.tk>
2016-01-05 14:36:01 +0800

    change saving directory to cwd/58data/

--- DataStorage.py
+++ DataStorage.py
@@ -7,18 +7,21 @@ import os
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
+path = os.getcwd()
 
 class StoreToFile(object):
     def storeData(self, city, company, contact_name, phone, description):
         #file_handler = open('D://DATA.txt', 'a')
-	file_handler = open('/Users/Roger/developer/58/58/DATA.html','a')
+	#file_handler = open('/Users/Roger/developer/58/58/DATA.html','a')
+	file_handler = open(path+'/58data/DATA.html', 'a')
         line = '<div>%s\t%s\t%s\t%s\n%s</div>\n' % (city, company, contact_name, phone, description)
         file_handler.write(line)
         file_handler.close()
 
     def storeFailtureURL(self, url):
         #file_handler = open('D://failure_url.txt', 'a')
-        file_handler = open('/Users/Roger/developer/58/58/failure_url.txt', 'a')
+        #file_handler = open('/Users/Roger/developer/58/58/failure_url.txt', 'a')
+        file_handler = open(path+'/58data/failure_url.txt', 'a')
         line = '%s\n' % url
         file_handler.write(line)
         file_handler.close()
